b8abe45114bf685a03dd2dca7de58d49d2c5bbbe
bkeny20 <62443745+bkeny20@users.noreply.github.com>
2026-05-13 21:09:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'beer-counter-v1';
+const LOCAL_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(LOCAL_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(event.request).then(cached => {
+        const networkFetch = fetch(event.request).then(response => {
+          if (response && response.status === 200) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        }).catch(() => null);
+
+        return cached || networkFetch || new Response('Offline', { status: 503 });
+      })
+    )
+  );
+});
